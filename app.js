34094e4633861c257d822a9ac0d15de4a0e41f17
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-07-08 01:22:56 +0530

    cors and / route updated

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const adminRoutes = require("./ROUTES/adminRoutes");
 const userRoutes = require("./ROUTES/userRoutes");
 
 // MIDDLEWARES
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(cors());
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.json({ message: "Server is healthy." });
+});
 app.use("/admin", adminRoutes);
 app.use("/api", userRoutes);
 
